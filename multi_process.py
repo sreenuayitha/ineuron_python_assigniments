8ed42772239bd75edc19413c47ab21b1e2f8cbef
sreenu ayitha <100477319+sreenuayitha@users.noreply.github.com>
2023-01-19 15:08:22 +0530

    Add files via upload

---
+++ multi_process.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import multiprocessing
+def now(seconds):
+        from datetime import datetime
+        from time import sleep
+        sleep(seconds)
+        print('wait', seconds, 'seconds, time is', datetime.utcnow())
+if __name__ == '__main__':
+        import random
+        for n in range(3):
+                seconds = random.random()
+                proc = multiprocessing.Process(target=now, args=(seconds,))
+                proc.start()
+
+
+# In[ ]:
+
+
+
+
